7a9da169d54078ba866263428e95e15a10c14ef9
Natarajan Sengoden <natarajansengoden@nataraj2.local>
2018-05-28 12:06:49 +0530

    Asset changed.

--- IMKnightsMove/View/IMGridCell.swift
+++ IMKnightsMove/View/IMGridCell.swift
@@ -15,13 +15,12 @@ class IMGridCell: UICollectionViewCell {
     func setupKnightCell(knight:IMKnight) {
         if knight.isKnight {
             knight.isAvailableToMove = false
-            self.imgView.image = UIImage.init(named: "knight")
-            self.bgView.backgroundColor = UIColor.init(red: 0.0/255.0, green: 122.0/255.0, blue: 255.0/255, alpha: 0.7) // For Indicating current knight position.
+            self.imgView.image = UIImage.init(named: "current-knight")
             knight.isKnight = false
         }else {
             self.bgView.backgroundColor = UIColor.clear
             if knight.isVisited {
-                self.imgView.image = UIImage.init(named: "knight")
+                self.imgView.image = UIImage.init(named: "")
             }else {
                 self.imgView.image = UIImage.init(named: "off")
             }
